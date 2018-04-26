5472733aa93d3f244e237e9df1438205de76d68e
SimePel <simepelmain@gmail.com>
2018-04-26 17:08:50 +0700

    Add solution of the base problem

---
+++ url-redirecter/handlers.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if v, ok := pathsToUrls[r.URL.Path]; ok {
+			h := http.RedirectHandler(v, http.StatusPermanentRedirect)
+			h.ServeHTTP(w, r)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+// PathsAndURLs used to unmarshal yaml into struct
+type PathsAndURLs struct {
+	Path string
+	URL  string `yaml:"url"`
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsed := make([]PathsAndURLs, 0)
+	err := yaml.Unmarshal(yml, &parsed)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsed)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func buildMap(parsed []PathsAndURLs) map[string]string {
+	m := make(map[string]string, len(parsed))
+	for _, v := range parsed {
+		m[v.Path] = v.URL
+	}
+	return m
+}
---
+++ url-redirecter/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/gh":         "https://github.com",
+		"/yaml-godoc": "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the fallback
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /h
+  url: https://habr.com
+`
+	yamlHandler, err := YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		log.Fatalln("YAMLHandler: ", err)
+	}
+
+	log.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
