038583a1277916669fe678235b4d7a6aabea06a1
SimePel <simepelmain@gmail.com>
2018-07-09 16:51:29 +0700

    Add phone numbers to db

--- phone-number-normalizer/main.go
+++ phone-number-normalizer/main.go
@@ -30,4 +30,25 @@ func main() {
 		log.Fatal("ping:", err)
 	}
 	fmt.Println("Successfully connected!")
+
+	phones := [8]string{
+		"1234567890",
+		"123 456 7891",
+		"(123) 456 7892",
+		"(123) 456-7893",
+		"123-456-7894",
+		"123-456-7890",
+		"1234567892",
+		"(123)456-7892",
+	}
+	insertStatement := `
+INSERT INTO phone_numbers (phone)
+VALUES ($1)`
+
+	for _, v := range phones {
+		_, err = db.Exec(insertStatement, v)
+		if err != nil {
+			log.Fatal("exec:", err)
+		}
+	}
 }
