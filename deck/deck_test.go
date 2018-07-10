a2109a16b13a013e51187d1b91e28a6a41680ae4
SimePel <simepelmain@gmail.com>
2018-07-10 19:01:06 +0700

    Add tests to New func

--- deck/deck_test.go
+++ deck/deck_test.go
@@ -2,6 +2,38 @@ package deck
 
 import "testing"
 
+func TestNew(t *testing.T) {
+	cards := New()
+	expected := []Card{
+		Card{
+			Rank: ace,
+			Suit: clubs,
+		},
+		Card{
+			Rank: two,
+			Suit: clubs,
+		},
+		Card{},
+		Card{},
+		Card{},
+		Card{},
+		Card{},
+		Card{},
+		Card{},
+		Card{},
+		Card{},
+		Card{},
+		Card{},
+		Card{
+			Rank: ace,
+			Suit: diamonds,
+		},
+	}
+	if cards[0] != expected[0] || cards[1] != expected[1] || cards[13] != expected[13] {
+		t.Fatalf("Got:\n%v\nWant:\n%v\n", cards, expected)
+	}
+}
+
 func TestRankString(t *testing.T) {
 	a := ace.String()
 	expected := "ace"
